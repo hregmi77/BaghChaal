e32f66aed5c5a2f8ef2cfc10d55076d81b9b64e6
Hem <hregmi@email.sc.edu>
2021-10-26 23:14:50 -0400

    New updates

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+from Game import Game
+from Player import MCTSPlayer,HumanPlayer
+from policy_value_net import PolicyValueNet
+from utils import symmetry_board_moves
+game=Game()
+goat=HumanPlayer()
+pvnet=PolicyValueNet("models/model.h5")
+pvnet_fn=pvnet.policy_value_fn
+bagh=MCTSPlayer(pvnet_fn,n_playout=500)
+data=game.start_play(bagh,goat)
+data=[x for x in data]
+data=symmetry_board_moves(data)
+state_batch = [x[0] for x in data]
+mcts_probs_batch = [x[1] for x in data]
+winner_batch = [x[2] for x in data]
+pvnet.train(state_batch,mcts_probs_batch,winner_batch,5)
+pvnet.save_model("model.h5")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hii, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
