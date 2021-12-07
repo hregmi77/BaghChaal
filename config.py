470bbae0e3c6be8b622666afb04023b3eafe2e05
rojina <rojinapanta99@gmail.com>
2021-12-06 21:06:17 -0500

    Chane in config

--- config.py
+++ config.py
@@ -24,25 +24,10 @@ class TrainConfig:
         self.ld=0.8
         self.elo_baserating = 1500
         self.elo_k_factor = 32
-        self.num_of_games = 100
+        self.num_of_games = 1000
         self.num_of_random_minmax_games = 100
         self.num_of_minmax_minmax_games = 50
         self.allow_pretrain = 1
         self.selfplay_dnn_update_interval = 1
-
-        #c_puct=0.0001 ld =0.8
-class TrainGreedyConfig:
-    def __init__(self):
-        self.learn_rate = 2e-3
-        self.lr_multiplier = 1.0  # adaptively adjust the learning rate based on KL
-        self.temp = 0.8  # the temperature param
-        self.n_playout = 800  # num of simulations for each move
-        self.c_puct = 1
-        self.buffer_size = 10000
-        self.batch_size = 1  # mini-batch size for training
-
-        self.play_batch_size = 1 # how many games to play
-        self.epochs = 7  # num of train_steps for each update
-        self.ld=0.8
 
         #c_puct=0.0001 ld =0.8
